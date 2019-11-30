f665982fd2eecebdd794f118a8fc77f7a861fec8
github username <github email address>
2019-11-30 11:40:58 -0500

    changes added

--- app/controllers/snacks_controller.rb
+++ app/controllers/snacks_controller.rb
@@ -2,11 +2,22 @@ class SnacksController < ApplicationController
   
 
   def home
-  
+
   end 
 
   def index
   	@snacks = Snack.all 
   end
+
+  def show
+  	@snack = Snack.find(params[:id])
+  end 
+
+  def new
+  end 
+
+  def create
+  end 
+  
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   
-  get '/' , to: "snacks#home"
-  get 'snacks' , to: "snacks#index"
+  get '/home' , to: "snacks#home" , as:"home"
+  # get '/snacks' , to: "snacks#index", as:"snacks"
+  # get '/snacks/:id' , to: "snacks#show" , as: "snack"
+
+  resources :snacks , only: [:index , :show , :new , :create ]
 
 end
